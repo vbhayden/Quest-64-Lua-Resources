5e599806408bde911d4b21c9fda2535a9741ffe0
Trey Hayden <vbhayden3@gmail.com>
2025-04-14 18:05:55 -0400

    fixing >> 2 typo

--- python/rng.py
+++ python/rng.py
@@ -125,7 +125,7 @@ def find_valid_heal_seeds(heal_results, max_hp, water_level, healing_rank, scan_
     
     valid_seeds = []
     bonus_healing = math.floor(bonus_coefficient * max_hp)
-    water_influence = water_level >> 2
+    water_influence = water_level >> 1
     
     min_heal = water_influence + bonus_healing
     heal_range = min_heal >> 2
@@ -160,26 +160,26 @@ def main():
 
     # prepare_lookups()
 
-    start = time.time()
+    # start = time.time()
 
-    # seeds = find_valid_heal_seeds([31, 36, 36, 34, 31, 33, 36], max_hp=158, water_level=38, healing_rank=2, scan_size=0x000FFFFF)
-    # seeds = find_valid_heal_seeds([23, 26, 22, 25, 23, 26, 26, 23, 24, 22, 26, 25], max_hp=158, water_level=38, healing_rank=1, scan_size=0x00FFFFFF)
-    # seeds = find_valid_heal_seeds([24, 23, 23, 22, 26, 26, 25, 26, 22, 22, 25, 22], max_hp=158, water_level=38, healing_rank=1, scan_size=0x00FFFFFF)
-    seeds = find_valid_heal_seeds(
-        heal_results=[24, 22, 24, 22, 24, 24, 25, 23, 25, 23, 25, 24], 
-        max_hp=158, 
-        water_level=38, 
-        healing_rank=1, 
-        scan_start=0x00000000,
-        scan_length=0xFFFFFFFF
-    )
+    # # seeds = find_valid_heal_seeds([31, 36, 36, 34, 31, 33, 36], max_hp=158, water_level=38, healing_rank=2, scan_size=0x000FFFFF)
+    # # seeds = find_valid_heal_seeds([23, 26, 22, 25, 23, 26, 26, 23, 24, 22, 26, 25], max_hp=158, water_level=38, healing_rank=1, scan_size=0x00FFFFFF)
+    # # seeds = find_valid_heal_seeds([24, 23, 23, 22, 26, 26, 25, 26, 22, 22, 25, 22], max_hp=158, water_level=38, healing_rank=1, scan_size=0x00FFFFFF)
+    # seeds = find_valid_heal_seeds(
+    #     heal_results=[24, 22, 24, 22, 24, 24, 25, 23, 25, 23, 25, 24], 
+    #     max_hp=158, 
+    #     water_level=38, 
+    #     healing_rank=1, 
+    #     scan_start=0x00000000,
+    #     scan_length=0xFFFFFFFF
+    # )
     
-    stop = time.time()
+    # stop = time.time()
 
-    for seed in seeds:
-        print(f"- {seed:8X}")
+    # for seed in seeds:
+    #     print(f"- {seed:8X}")
 
-    print(f"Elapsed: {stop - start:.2f}, Valid Seeds: {len(seeds)}")
+    # print(f"Elapsed: {stop - start:.2f}, Valid Seeds: {len(seeds)}")
 
     # # (rng, healing) = simulate_healing_1(0x00000000, water_level=38, max_hp=158)
     # # print(f"{rng:8X}, {healing}")
